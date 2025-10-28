bcc01e65fd9afa2c4f808c0e16b415f74c648a56
mohamed altahan <medo85559@gmail.com>
2025-10-28 21:18:34 +0300

    .

---
+++ AlkhabeerAccountant/Helpers/Converters/RadioTagMatchConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AlkhabeerAccountant.Helpers.Converters
+{
+    public class RadioTagMatchConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            MessageBox.Show("تم حفظ البيانات بنجاح!");
+            return value?.ToString() == parameter?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            MessageBox.Show("تم حفظ البيانات بنجاح!");
+
+            if ((bool)value)
+                return parameter?.ToString();
+            return Binding.DoNothing;
+        }
+    }
+}
